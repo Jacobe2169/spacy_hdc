83b68d456951126f0b63e243f2ee8d5cb72e0698
Fize Jacques <jacques.fize@cirad.fr>
2019-01-29 16:45:34 +0100

    Debug

--- spacy_hdc/corpus.py
+++ spacy_hdc/corpus.py
@@ -6,13 +6,44 @@ import pandas as pd
 import numpy as np
 import sys, os, re, json, glob
 import textwrap
+import pickle
 
 from .helpers.filter import clean_text
 from spacy.language import Language as SpacyLang
 tqdm.monitor_interval=0
 
-from textacy import Corpus
+from spacy.attrs import LOWER, POS, ENT_TYPE, IS_ALPHA, DEP, LEMMA, LOWER, IS_PUNCT, IS_DIGIT, IS_SPACE, IS_STOP
+from spacy.tokens import Doc
 
+class Corpus():
+
+    def __init__(self,name,text_list,lang):
+        self.name=name
+        self.nlp=lang
+        self.text_list=text_list
+        self.doc_bytes=[]
+        self.text_bytes=[]
+        self.vocab_bytes=None
+        self.noun_chunks=[]
+
+    def extract_data(self,attr=[LOWER, POS, ENT_TYPE, IS_ALPHA, DEP, LEMMA, LOWER, IS_PUNCT, IS_DIGIT, IS_SPACE, IS_STOP]):
+        for doc in self.nlp.pipe(self.text_list,batch_size=100, n_threads=4):
+            self.doc_bytes.append(doc.to_array(attr))
+            self.text_bytes.append([t.text for t in doc])
+            self.noun_chunks.append([[nc.text,nc.start,nc.end] for nc in doc.noun_chunks])
+        self.vocab_bytes=self.nlp.vocab.to_bytes()
+
+    def save(self):
+        with open("{0}.pickle".format(self.name),"wb") as handle:
+            pickle.dump((self.doc_bytes, self.vocab_bytes,self.text_bytes,self.noun_chunks),handle)
+            #doc_bytes = [doc.to_array([LOWER, POS, ENT_TYPE, IS_ALPHA,DEP]) for doc in docs]
+    @staticmethod
+    def load(fn,nlp):
+        with open(fn, "rb") as handle:
+            doc_bytes, vocab_bytes,text_bytes,noun_chunks = pickle.load(handle)
+            nlp.vocab.from_bytes(vocab_bytes)
+            docs = [Doc(nlp.vocab,words=text_bytes[i]).from_array([LOWER, POS, ENT_TYPE, IS_ALPHA,DEP],b) for i,b in enumerate(doc_bytes)]
+        return docs,noun_chunks
 class CorpusHDC:
 
     def __init__(self,text_df,name,lang,chunk_size=500):
@@ -54,9 +85,10 @@ class CorpusHDC:
         prev_chunk=0
         for chunk in tqdm(chunks):
             next_chunk_size=prev_chunk+len(chunk)
-            fn="{2}_{0}to{1}.pkl".format(prev_chunk,next_chunk_size,self.name)
-            cor_en=Corpus(self.nlp,chunk.tolist())
-            cor_en.save(fn)
+            fn="{2}_{0}to{1}".format(prev_chunk,next_chunk_size,self.name)
+            cor_en=Corpus(fn,chunk.tolist(),self.nlp)
+            cor_en.extract_data()
+            cor_en.save()
             prev_chunk=next_chunk_size
     
     def save(self):
